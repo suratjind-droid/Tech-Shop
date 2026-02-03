bea733dd77398fa3c69ae23339113f580181a4c2
SBACNON <SBACNON@1508-9>
2026-02-03 10:01:39 +0700

    Add project files.

---
+++ D3/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace D3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Leelawadee UI", 20F);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(249, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Tech Shop Program";
+            label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(373, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(477, 399);
+            dataGridView1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Location = new Point(12, 61);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(283, 399);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "ข้อมูลสินค้า";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 3;
+            label2.Text = "ชื่อสินค้า";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 15);
+            label3.TabIndex = 4;
+            label3.Text = "ประเภท";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(29, 15);
+            label4.TabIndex = 5;
+            label4.Text = "ราคา";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 125);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 15);
+            label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 125);
+            label6.Name = "label6";
+            label6.Size = new Size(62, 15);
+            label6.TabIndex = 7;
+            label6.Text = "จำนวนสินค้า";
+            label6.Click += label6_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "ขาย";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 193);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "นำเข้า";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(6, 233);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 10;
+            button3.Text = "ส่งออก";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(6, 271);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 11;
+            button4.Text = "เพิ่มสินค้า";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(87, 157);
+            label7.Name = "label7";
+            label7.Size = new Size(37, 15);
+            label7.TabIndex = 12;
+            label7.Text = "จำนวน";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(87, 197);
+            label8.Name = "label8";
+            label8.Size = new Size(37, 15);
+            label8.TabIndex = 13;
+            label8.Text = "จำนวน";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(87, 237);
+            label9.Name = "label9";
+            label9.Size = new Size(37, 15);
+            label9.TabIndex = 14;
+            label9.Text = "จำนวน";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(87, 275);
+            label10.Name = "label10";
+            label10.Size = new Size(37, 15);
+            label10.TabIndex = 15;
+            label10.Text = "จำนวน";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(6, 313);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 16;
+            button5.Text = "ลบสินค้า";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(158, 313);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 17;
+            button6.Text = "ล้าง";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(6, 355);
+            button7.Name = "button7";
+            button7.Size = new Size(118, 23);
+            button7.TabIndex = 18;
+            button7.Text = "ค้นหา";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(158, 355);
+            button8.Name = "button8";
+            button8.Size = new Size(110, 23);
+            button8.TabIndex = 19;
+            button8.Text = "แสดงสินค้าทั้งหมด";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(74, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(148, 23);
+            textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(74, 90);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(148, 23);
+            textBox2.TabIndex = 21;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(74, 122);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(148, 23);
+            textBox3.TabIndex = 22;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(74, 58);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(148, 23);
+            comboBox1.TabIndex = 23;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(130, 151);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(103, 23);
+            textBox4.TabIndex = 24;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(130, 193);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(103, 23);
+            textBox5.TabIndex = 25;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(130, 233);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(103, 23);
+            textBox6.TabIndex = 26;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(130, 272);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(103, 23);
+            textBox7.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(902, 533);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label6;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private ComboBox comboBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+    }
+}
---
+++ D3/Form1.cs
@@ -0,0 +1,40 @@
+namespace D3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
